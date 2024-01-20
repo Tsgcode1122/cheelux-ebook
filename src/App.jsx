ff3e3f493cc60432622a97f1e80d1c486e79d39b
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-01-19 23:04:27 -0800

    netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Ebook from "./pages/Ebook";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Ebook from "../public/Ebook";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Ebook" element={<Ebook />} />
+        <Route
+          path="/"
+          element={<Navigate to="/EBook" />} // Redirect to /EBook when accessing root path
+        />
+        <Route path="/EBook" element={<Ebook />} />
       </Routes>
     </BrowserRouter>
   );
