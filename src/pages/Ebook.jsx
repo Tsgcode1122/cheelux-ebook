1a3ecf7e5d283db23b07c3bc503d35f318d1f739
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-01-19 23:17:31 -0800

    redirect

---
+++ src/pages/Ebook.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbars from "../src/component/Navbar";
+import Footer from "../src/component/Footer";
+import LandingPage from "../src/component/LandingPage";
+import ContactForm from "../src/component/ContactForm";
+
+const Ebook = () => {
+  return (
+    <>
+      <Navbars />
+      <LandingPage />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+};
+
+export default Ebook;
