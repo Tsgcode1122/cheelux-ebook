0a86c3a841087a5728fd8c0c7480c3d3c2859072
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-01-19 22:46:06 -0800

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Ebook from "./pages/Ebook";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Ebook />} />
-        <Route path="/" element={<Ebook />} />
+        <Route
+          path="/"
+          element={<Navigate to="/EBook" />} // Redirect to /EBook when accessing root path
+        />
         <Route path="/EBook" element={<Ebook />} />
       </Routes>
     </BrowserRouter>
