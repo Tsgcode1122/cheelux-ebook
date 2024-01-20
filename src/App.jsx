937042cdfaef6e2ceac5058ebe552aff1c058360
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-01-19 21:17:10 -0800

    /ebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./component/LandingPage";
 import ContactForm from "./component/ContactForm";
 import Navbars from "./component/Navbar";
 import Footer from "./component/Footer";
-
+import Ebook from "./pages/Ebook";
 const App = () => {
   return (
-    <>
-      <Navbars />
-      <LandingPage />
-      <ContactForm />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Ebook />} />
+        <Route path="/" element={<Ebook />} />
+        <Route path="EBook" element={<Ebook />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/Ebook.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import LandingPage from "../component/LandingPage";
+import ContactForm from "../component/ContactForm";
+import Navbars from "../component/Navbar";
+import Footer from "../component/Footer";
+
+const Ebook = () => {
+  return (
+    <>
+      <Navbars />
+      <LandingPage />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+};
+
+export default Ebook;
