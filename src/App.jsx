4cc38690f79d9bb7a274639f40e8e22ceecf04ed
Tsgcode1122 <Tsgcode1122@users.noreply.github.com>
2024-01-19 22:52:18 -0800

    ebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ebook from "./pages/Ebook";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Navigate to="/EBook" />} // Redirect to /EBook when accessing root path
-        />
-        <Route path="/EBook" element={<Ebook />} />
+        <Route path="/Ebook" element={<Ebook />} />
       </Routes>
     </BrowserRouter>
   );
